de3d7e45004953bc267242455e28f1ebc7c1dbf3
George Zhang <geetransit@gmail.com>
2022-04-06 19:36:24 -0400

    Fix: mismatching types

--- Point.py
+++ Point.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from copy import copy
 from dataclasses import dataclass
 
+from Vector import Vector
+
 @dataclass
 class Point:
     x: int = 0
@@ -18,22 +20,22 @@ class Point:
     def getMidpoint(p: Point, q: Point) -> Point:
         return Point((p.x+q.x)//2, (p.y+q.y)//2, (p.z+q.z)//2)
 
-    def __iadd__(self, other: Point) -> Point:
+    def __iadd__(self, other: Vector) -> Point:
         self.x += int(other.x)
         self.y += int(other.y)
         self.z += int(other.z)
         return self
-    def __add__(self, other: Point) -> Point:
+    def __add__(self, other: Vector) -> Point:
         new = copy(self)
         new += other
         return new
 
-    def __isub__(self, other: Point) -> Point:
+    def __isub__(self, other: Vector) -> Point:
         self.x -= int(other.x)
         self.y -= int(other.y)
         self.z -= int(other.z)
         return self
-    def __sub__(self, other: Point) -> Point:
+    def __sub__(self, other: Vector) -> Point:
         new = copy(self)
         new -= other
         return new
--- Polygon.py
+++ Polygon.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from copy import copy
 from dataclasses import dataclass, field
+from typing import List
 
+from Vector import Vector
 from Point import Point
 
 @dataclass
 class Polygon:
-    vertices: list = field(default_factory=list)
+    vertices: List[Point] = field(default_factory=list)
 
     def __iadd__(self, other: Vector) -> Polygon:
         for vertex in self.vertices:
